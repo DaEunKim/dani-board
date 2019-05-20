19a0310909820bc9523149852142aa582761ccfc
김다은 <gimda-eun@gimda-eun-ui-iMac.local>
2019-05-20 12:10:54 +0900

    delete blank

--- src/BoardItem.js
+++ src/BoardItem.js
@@ -1,30 +1,49 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class BoardItem extends Component {
-    handleRemove = () => {
-        const { row, onRemove } = this.props;
-        onRemove(row.brdno);
-    }    
-    
-    handleSelectRow = () => {
-        const { row, onSelectRow } = this.props;
-        onSelectRow(row);
-    }    
-    
-    render() {
-        const link_tmp = this.props.row;
-        return(
-            <tr key={link_tmp.brdno}>
-                <td><Link to={'/user/' + link_tmp.brdno}>{link_tmp.brdno} </Link></td>
-                <td><Link to={'/user/' + link_tmp.brdno}><div onClick={this.handleSelectRow}> {link_tmp.brdtitle}</div></Link></td>
-                <td><Link to={'/user/' + link_tmp.brdno}>{link_tmp.brdwriter} </Link></td>
-                <td><Link to={'/user/' + link_tmp.brdno}> {link_tmp.brddate.toLocaleDateString('ko-KR')}</Link></td>
-                <td><button onClick={this.handleRemove}> X </button></td>
-            </tr>
-        );
-    }
+  handleRemove = () => {
+    const { row, onRemove } = this.props;
+    onRemove(row.brdno);
+  };
+
+  handleSelectRow = () => {
+    const { row, onSelectRow } = this.props;
+    onSelectRow(row);
+  };
+
+  render() {
+    const link_tmp = this.props.row;
+    return (
+      <tr key={link_tmp.brdno}>
+        <td>
+          <Link to={"/user/" + link_tmp.brdno}>
+            <div onClick={this.handleSelectRow}>{link_tmp.brdno} </div>
+          </Link>
+        </td>
+        <td>
+          <Link to={"/user/" + link_tmp.brdno}>
+            <div onClick={this.handleSelectRow}> {link_tmp.brdtitle}</div>
+          </Link>
+        </td>
+        <td>
+          <Link to={"/user/" + link_tmp.brdno}>
+            <div onClick={this.handleSelectRow}>{link_tmp.brdwriter} </div>
+          </Link>
+        </td>
+        <td>
+          <Link to={"/user/" + link_tmp.brdno}>
+            <div onClick={this.handleSelectRow}>
+              {link_tmp.brddate.toLocaleDateString("ko-KR")}
+            </div>
+          </Link>
+        </td>
+        <td>
+          <button onClick={this.handleRemove}> X </button>
+        </td>
+      </tr>
+    );
+  }
 }
 
 export default BoardItem;
-
--- src/DaniBoard.js
+++ src/DaniBoard.js
@@ -1,145 +1,154 @@
-import React, { Component } from 'react';
-import BoardForm from './BoardForm';
-import BoardItem from './BoardItem';
-import { Route, BrowserRouter as Router , Link } from 'react-router-dom';
-import './App.css';
+import React, { Component } from "react";
+import BoardForm from "./BoardForm";
+import BoardItem from "./BoardItem";
+import { Route, BrowserRouter as Router, Link } from "react-router-dom";
+import "./App.css";
 
 class DaniBoard extends Component {
-    state = {
-        maxNo: 6,
-        boards: [
-            {
-                brdno: 1,
-                brdwriter: 'dani',
-                brdtitle: 'React Programming',
-                brddate: new Date()
-            },
-            {
-                brdno: 2,
-                brdwriter: 'Kim Da Eun',
-                brdtitle: 'Web Programming',
-                brddate: new Date()
-            },
-            {
-                brdno: 3,
-                brdwriter: '김다은',
-                brdtitle: '유니타스',
-                brddate: new Date()
-            },
-            {
-                brdno: 4,
-                brdwriter: '다은',
-                brdtitle: 'ST',
-                brddate: new Date()
-            },
-            {
-                brdno: 5,
-                brdwriter: 'rlaekdms',
-                brdtitle: 'good',
-                brddate: new Date()
-            }
-        ],
-         selectedBoard:{}
-    }
-    
-    handleSaveData = (data) => {
-        
-        if (!data.brdno) { 
-            this.setState({
-                maxNo: this.state.maxNo+1,
-                boards: this.state.boards.concat(
-                    {brdno: this.state.maxNo, brddate: new Date(), ...data }
-                    ),
-                selectedBoard: {}
-            });
-        } else {
-            this.setState({
-                boards: this.state.boards.map(row => data.brdno === row.brdno ? {...data }: row),
-                selectedBoard: {}
-            })            
-        }
-    }
-    
-    handleRemove = (brdno) => {
+  state = {
+    maxNo: 6,
+    boards: [
+      {
+        brdno: 1,
+        brdwriter: "dani",
+        brdtitle: "React Programming",
+        brddate: new Date()
+      },
+      {
+        brdno: 2,
+        brdwriter: "Kim Da Eun",
+        brdtitle: "Web Programming",
+        brddate: new Date()
+      },
+      {
+        brdno: 3,
+        brdwriter: "김다은",
+        brdtitle: "유니타스",
+        brddate: new Date()
+      },
+      {
+        brdno: 4,
+        brdwriter: "다은",
+        brdtitle: "ST",
+        brddate: new Date()
+      },
+      {
+        brdno: 5,
+        brdwriter: "rlaekdms",
+        brdtitle: "good",
+        brddate: new Date()
+      }
+    ],
+    selectedBoard: {}
+  };
+
+  handleSaveData = data => {
+    if (!data.brdno) {
+      if (data.brdtitle === "") {
+        window.alert("제목을 입력하세요. ");
+      } else if (data.brdwriter === "") {
+        window.alert("이름을 쓰세요 ");
+      } else {
         this.setState({
-            maxNo : this.state.maxNo-1,
-            boards: this.state.boards.filter(row => row.brdno !== brdno)
-            // , selectedBoard: {}
-        })
-    }
-    
-    handleSelectRow = (row) => {
-        this.setState({
-            selectedBoard:row
+          maxNo: this.state.maxNo + 1,
+          boards: this.state.boards.concat({
+            brdno: this.state.maxNo,
+            brddate: new Date(),
+            ...data
+          }),
+          selectedBoard: {}
         });
+      }
+    } else {
+      this.setState({
+        boards: this.state.boards.map(row =>
+          data.brdno === row.brdno ? { ...data } : row
+        ),
+        selectedBoard: {}
+      });
     }
-    
-    render() {
-        const { boards, selectedBoard } = this.state;
-        const list = boards.map(row => (
-            
-            <BoardItem key={row.brdno} 
-                row={row} 
-                onRemove={this.handleRemove} 
-                onSelectRow={this.handleSelectRow} /> 
-                
-        
-        ));
+  };
+  handleRemove = brdno => {
+    this.setState({
+      maxNo: this.state.maxNo,
+      boards: this.state.boards.filter(row => row.brdno !== brdno),
+      selectedBoard: {}
+    });
+  };
 
+  handleSelectRow = row => {
+    this.setState({ selectedBoard: row });
+  };
 
-        return (
-            <Router>
-            <div className="cusAppCss">
-            <h2>Dani Board</h2>            
-                <BoardForm selectedBoard={selectedBoard} onSaveData={this.handleSaveData}/>
-                <table border="3px">
-                    <tbody align="center">
-                    <tr>
-                        <td className="Index" width="50">Index</td>
-                        <td width="300">Title</td>
-                        <td width="200">Name</td>
-                        <td width="300">Date</td>
-                    </tr>
-                    {list}
-                    </tbody>
-                </table>
-                <Route path="/user/:id" component={()=><UserCard ListState={this.state}/>} />
-                
-            </div>
-            
+  render() {
+    const { boards, selectedBoard } = this.state;
+    const list = boards.map(row => (
+      <BoardItem
+        key={row.brdno}
+        row={row}
+        onRemove={this.handleRemove}
+        onUpdate={this.handleUpdate}
+        onSelectRow={this.handleSelectRow}
+      />
+    ));
 
-            </Router>
-        );
-    }
+    return (
+      <Router>
+        <div className="cusAppCss">
+          <h2>Dani Board</h2>
+          <BoardForm
+            selectedBoard={selectedBoard}
+            onSaveData={this.handleSaveData}
+          />
+          <table border="3px">
+            <tbody align="center">
+              <tr className="table">
+                <td width="50">Index</td>
+                <td width="300">Title</td>
+                <td width="200">Name</td>
+                <td width="300">Date</td>
+              </tr>
+              {list}
+            </tbody>
+          </table>
+          <Route
+            path="/user/:id"
+            component={() => <UserCard ListState={this.state} />}
+          />
+        </div>
+      </Router>
+    );
+  }
 }
 
 class UserCard extends Component {
-    render () {
-    const {ListState} = this.props;
+  render() {
+    const { ListState } = this.props;
     const boardVal = ListState.boards;
-    const id = ListState.selectedBoard.brdno;
+    let id = ListState.selectedBoard.brdno;
 
-    // console.log(boardVal[4].brdtitle);
-    
-    if(id==null || id-1==null){
-        return (
-            <div align="center">
-            <div >내용이 부족합니다. </div>
-          <button><Link to='/'>뒤로가기</Link></button>
+    if (id == null || id - 1 == null) {
+      return (
+        <div className="table" align="center">
+          <div>뒤로가기를 누르세요. </div>
+          <button>
+            <Link to="/">뒤로가기</Link>
+          </button>
         </div>
-        )
-    }
-    else{
-        return (
-            <div align="center">
-                <div >{id}. {boardVal[id-1].brdtitle} - {boardVal[id-1].brdwriter} </div>
-              <button><Link to='/'>뒤로가기</Link></button>
-            </div>
-          )
-        }
-    
+      );
+    } else {
+      return (
+        <div className="table" align="center">
+          <div>
+            {id}. {boardVal[id - 1].brdtitle} - {boardVal[id - 1].brdwriter}{" "}
+          </div>
+          <button>
+            <Link to="/">뒤로가기</Link>
+          </button>
+        </div>
+      );
     }
   }
+}
 
 export default DaniBoard;
-
